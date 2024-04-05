88a739aec10814d5198fbff728dba79ae64cdf8c
Shield Salcedo <115688089+shield0225@users.noreply.github.com>
2024-04-05 16:36:08 -0400

    Update ContactUs.js

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-//
-// this is the home page component
+
+
 function Home(props) {
   return (
     <div>
@@ -12,6 +12,4 @@ function Home(props) {
     </div>
   );
 }
-// withRouter will pass updated match, location, and history props
-// to the wrapped component whenever it renders.
 export default Home;
