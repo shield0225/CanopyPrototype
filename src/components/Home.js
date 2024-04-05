d08b0a3c2a9740332f44a90f7444de1c9fd7ae55
Shield Salcedo <115688089+shield0225@users.noreply.github.com>
2024-04-05 16:37:27 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,7 @@ import NaviBar from "./NaviBar";
 import Footer from "./Footer";
 import ImageWithText from "./ImageWithText";
 import Solutions from "./Solutions";
-//
-// this is the home page component
+
 function Home(props) {
   return (
     <>
@@ -17,6 +16,5 @@ function Home(props) {
     </>
   );
 }
-// withRouter will pass updated match, location, and history props
-// to the wrapped component whenever it renders.
+
 export default Home;
