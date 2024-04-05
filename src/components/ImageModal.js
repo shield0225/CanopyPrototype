d3fdb9c15b21525b4e159bd021ad92ca1870b5ac
Shield Salcedo <115688089+shield0225@users.noreply.github.com>
2024-04-05 16:37:49 -0400

    Update ImageModal.js

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -1,36 +1,5 @@
 import React, { useState } from "react";
-import "./Modal.css"; // Make sure to create a Modal.css file with the CSS provided earlier
-
-// function ImageModal() {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const openModal = () => setIsOpen(true);
-//   const closeModal = () => setIsOpen(false);
-
-//   return (
-//     <>
-//       <button onClick={openModal}>Open Modal</button>
-
-//       {isOpen && (
-//         <div className="modal" onClick={closeModal}>
-//           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-//             <span className="close" onClick={closeModal}>
-//               &times;
-//             </span>
-//             <img
-//               src="ttps://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/River_of_Blue.jpg/1280px-River_of_Blue.jpg"
-//               alt="Descriptive Alt Text"
-//               style={{ maxWidth: "100%" }}
-//             />
-//             <p>Some text description can go here if needed.</p>
-//           </div>
-//         </div>
-//       )}
-//     </>
-//   );
-// }
-
-// export default ImageModal;
+import "./Modal.css"; 
 
 function ImageModal({ isOpen, closeModal, imgSrc, altText, description }) {
   if (!isOpen) return null;
